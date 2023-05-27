e223b80d7eedca660470b283feb45fabbb112608
Yohannes19 <joeabrha@gmail.com>
2023-05-28 00:58:21 +0200

    db pg_sq

--- database/db.js
+++ database/db.js
@@ -1,11 +1,11 @@
-const Pool= require("pg").Pool;
+// const Pool= require("pg").Pool;
 
-const pool= new Pool({
-    user :"postgres",
-    password:"Meron",
-    host:"localhost",
-    port:"5432",
-    database :"HealthGIS"
-});
+// const pool= new Pool({
+//     user :"postgres",
+//     password:"Meron",
+//     host:"localhost",
+//     port:"5432",
+//     database :"HealthGIS"
+// });
 
-module.exports = pool;
+// module.exports = pool;
