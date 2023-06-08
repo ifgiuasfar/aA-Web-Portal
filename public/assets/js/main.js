6f52ebf5e69f3355bbb36d8839f08d9629856cfe
Yohannes19 <joeabrha@gmail.com>
2023-06-08 21:46:51 +0200

    adding wms layer

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,113 +1,126 @@
+var map = L.map('map').setView([51.9451804, 7.5720384], 17);
 
-var map = L.map('map',{ 
-       center: [51.9936,7.4857] ,
-       zoom:16
-      });
 var streets= L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 })
 var satellite = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
   attribution: 'Map data © Esri'
 });
+//var Dem=L.tileLayer('http://localhost:9000/geoserver-proxy?service=WMS&version=1.1.0&request=GetMap&layers=cite:Orthomosaic_Multispectral&bbox=7.570063780490534,51.943590542913974,7.574740819568534,51.94677105570398&width=768&height=522&srs=EPSG:4326&format=image/jpeg&STYLES=raster')
 var basemaps = {
+    'Satellite': satellite,
     'Streets': streets,
-    'Satellite': satellite
-  };
-  streets.addTo(map); // Set a default basemap layer
-  L.control.layers(basemaps).addTo(map);
-
-//map.locate({setView: true, maxZoom: 80});
-
-// Show a market at the position of the Eiffel Tower
-//let rivrea = L.marker([ -1.2960,36.8050]).addTo(map);
-//rivrea.bindPopup("River Aa- Proposed Project").openPopup();
-//Map scale showup
-L.control.scale({metric:true}).addTo(map);
-//zoom purpose
-
-
-
-//sample geoJSON
-// fetch('http://localhost:9000/data/api/healthfacilities',{
-//   method:'GET',
-//   headers:{
-//     'Accept':'application/json',
-//   },
-// })
-//   .then(response=>response.json())
-//   .then(data =>{
-//     console.log('success!', data);
-//    // const geoJSONData = data;
-
-//    const geoJSONLayer = L.geoJSON(data,{
-//     pointToLayer: function (feature, latlng) {
-//       const color = '#ff0000';
-//       const coordinates = feature.geometry.coordinates;
-//       const swappedCoordinates = [coordinates[1], coordinates[0]];
-//       const swappedLatlng = L.latLng(swappedCoordinates[0], swappedCoordinates[1]);
-
-//        return L.marker(swappedLatlng).
-//        setIcon(
-//         L.divIcon({
-//           className: 'custom-marker',
-//           iconSize: [20, 20], 
-//           iconAnchor: [10, 10], 
-//           html: '<div style="background-color: ' + color + ';"></div>'
-//         })
-//        ).bindPopup(feature.properties.name);
-//     }
-//     }).addTo(map);
   
+  };
+L.control.layers(basemaps).addTo(map);
+satellite.addTo(map); // Set a default basemap layer
+L.control.scale({metric:true}).addTo(map);
+//Leaflet Draw
+var drawnItems = new L.FeatureGroup();
+map.addLayer(drawnItems);
+var drawControl = new L.Control.Draw({
+    edit: {
+        featureGroup: drawnItems
+    }
+});
+map.addControl(drawControl);
+map.on('draw:created', function (e) {
+    var type = e.layerType,
+        layer = e.layer;
+        if (type === 'marker') {
+          layer.on('click', function (){
+            var marker=layer.getLatLngs();
+            layer.bindPopup('A new created popup!'+ marker);
+          })
+          
+        }
+        else if (type === 'rectangle') {
+          layer.on('mouseover', function() {
+              var mit=layer.getLatLngs()
+              layer.bindPopup('Bounding box of the rectangle'+mit)    
+          });
+      }
+      else if(type=='circle'){
+        layer.on('mouseover', function(){
+          var circle=layer.getLatLngs();
+          layer.bindPopup('Circle box,or radius' +circle )
+        })
+      }
+  
+    drawnItems.addLayer(layer);
+});
+rgb=L.tileLayer.wms("http://localhost:9000/geoserver-proxy", {
+    layers: 'cite:Aa_2022_RGB',
+    transparent:true,
+     opacity:0.5,
+    maxZoom: 20,
+    maxNativeZoom: 18,
+   format: 'image/png',   
+});
+
+
+ms=L.tileLayer.wms("http://localhost:9000/geoserver-proxy", {
+    layers: 'cite:Orthomosaic_Multispectral',
+    transparent:true,
+    //opacity:0.4,
+    maxZoom: 20,
+    zIndex:400,
+    maxNativeZoom: 18,
+   format: 'image/png',   
+});
+ms.addTo(map);
+dummy=L.ImageOvelay('./data/omms.jpg').addTo(map)
+
+// function showGetFeatureInfo(latlng,layer){
+//   var prop = layer.features[0].properties;
+//   var mark=L.marker([latlng.lat,latlng.lng]).addTo(map);
+//   var blue=prop['BLUE_BAND'];
+//   var green=prop['GREEN_BAND'];
+//   var red=prop['RED_BAND'];
+//   mark.bindPopup("Blue Band: "+blue.toString()+" <br>Green Band:"+green.toString()+"<br>Red Band:"+red.toString());
+//   document.querySelector('#demoR').innerHTML=red; document.querySelector('#demoB').innerHTML=blue; document.querySelector('#demoG').innerHTML = green; }
+
+//   map.on('click',function(e){
+//     showGetFeatureInfo(e.latlng,ms);
 //   })
-// .catch(error => {
-//   console.error('Error fetching GeoJSON data:', error);
-// });
-// var mslayer= 'http://localhost:8080/geoserver/cite/wms?service=WMS&version=1.1.0&request=GetMap&layers=cite%3AOrthomosaic_Multispectral&bbox=7.570063780490534%2C51.943590542913974%2C7.574740819568534%2C51.94677105570398&width=768&height=522&srs=EPSG%3A4326&styles=&format=application/openlayers';
-// //load wms form geoserver
-// const mywms = L.tileLayer.wms("http://localhost:8080/geoserver/cite/wms", {
-//     layers: 'cite:Orthomosaic_Multispectral',
-//     format: 'image/jpeg',
-//     transparent: true,
-//     crossOrigin:false,
-//     version: '1.1.0',
-//     attribution: "river layer multspectral"
-// });
-// mywms.addTo(map);
+  //layer Switching
+var layerRadio=document.getElementsByName('layerRadio')
+for(var i=0;i< layerRadio.length; i++){
+  layerRadio[i].addEventListener('change',function(){
+    var  selectedLayer=this.value;
+    if(selectedLayer==='rgb'){
+      map.removeLayer(ms);
+      rgb.addTo(map);
+    }
+    else if(selectedLayer==='multispectral'){
+      map.removeLayer(rgb);
+      ms.addTo(map)
+    }
+    else if(selectedLayer==='ms'){
+      map.removeLayer(rgb)
+      map.removeLayer(ms)
+      dummy.addTo(map)
+    }
+  })
+}
+//Transparency slider
+var  transparent_slider=document.getElementById('transparency-slider')
+transparent_slider.addEventListener('input',function(){
+  var opacity= this.value/100;
+  var activelayer=map.hasLayer(rgb)?rgb:ms;
+  activelayer.setOpacity(opacity);
+})
 
-const imageUrl = 'http://localhost:8080/geoserver/cite/wms?service=WMS&version=1.1.0&request=GetMap&layers=cite:Orthomosaic_Multispectral&bbox=7.570063780490534,51.943590542913974,7.574740819568534,51.94677105570398&width=768&height=522&srs=EPSG:4326&format=image/jpeg';
-const imageUrl1 = 'http://localhost:9000/geoserver-proxy?service=WMS&version=1.1.0&request=GetMap&layers=cite:Orthomosaic_Multispectral&bbox=7.570063780490534,51.943590542913974,7.574740819568534,51.94677105570398&width=768&height=522&srs=EPSG:4326&format=image/jpeg';
-const imageUrl2 = './data/omms.jpg';
-const bounds = [[51.943590542913974, 7.570063780490534],
-                [51.946771055703981,  7.574740819568534]];
-img = L.imageOverlay(imageUrl2, bounds  );
-img.filter('ndvi,colormap');
 
-img.addTo(map);
-L.rectangle(bounds).addTo(map);
- map.fitBounds(bounds);
+// const imageUrl1 = 'http://localhost:9000/geoserver-proxy?service=WMS&version=1.1.0&request=GetMap&layers=cite:Orthomosaic_Multispectral&bbox=7.570063780490534,51.943590542913974,7.574740819568534,51.94677105570398&width=768&height=522&srs=EPSG:4326&format=image/jpeg&STYLES=raster';
+// const rgb ='http://localhost:9000/geoserver-proxy?service=WMS&version=1.1.0&request=GetMap&layers=cite:Aa_2022_RGB&bbox=7.569617411262769,51.943382785237084,7.575163927539459,51.94707385427448&width=768&height=511&srs=EPSG:4326&styles=&format=image/jpeg';
+// const imageUrl2 = './data/omms.jpg';
+// const bounds = [[51.943590542913974, 7.570063780490534], [51.946771055703981,  7.574740819568534]];
+// img=L.imageOverlay(rgb,bounds);
+// //img.filter('ndvi,colormap');
+// img.addTo(map);
+// L.rectangle(bounds).addTo(map);
+// map.fitBounds(bounds);
+ // Initialize the FeatureGroup to store editable layers
 
-//const imageUrl1 = 'http://localhost:9000/geoserver-proxy?service=WMS&version=1.1.0&request=GetMap&layers=cite:Orthomosaic_Multispectral&bbox=7.570063780490534,51.943590542913974,7.574740819568534,51.94677105570398&width=768&height=522&srs=EPSG:4326&format=image/geotiff';
-
-// Continue with the rest of your code
-
-//  const imgUrl ="./assets/cite-Orthomosaic_Multispectral.tif";
-//  //const path= "../RGB/Aa_2022_RGB.tif"
-// const plottyRenderer = L.LeafletGeotiff.plotty({
-//   displayMin: 0,
-//   displayMax: 255,
-//   clampLow: false,
-//   clampHigh: false
-// });
-
-// const windSpeed = L.leafletGeotiff(imageUrl1,
-//   {
-//     renderer:plottyRenderer
-//   }).addTo(map);
-
-//   let drawnItems = new L.FeatureGroup();
-//   map.addLayer(drawnItems);
-//   map.on('draw:created', (e) => {
-//     // Each time we create a feature(point, line or polygon), we add this feature to the feature group wich is drawnItems in this case
-//     drawnItems.addLayer(e.layer);
-// });
 
